850edfb9a570c1ccbc067e55e3b7e0e22390357f
JozefZivcic <jozefzivcic@gmail.com>
2016-02-03 10:44:02 +0100

    Added method definitions in threadhandler

--- bp/threadhandler.cpp
+++ bp/threadhandler.cpp
@@ -1,16 +1,61 @@
 #include "threadhandler.h"
 
+using namespace std;
+
 ThreadHandler::ThreadHandler(int n) : number(n), endThreads(false)
 {
-    tests.reserve(n);
-    //vars.reserve(n);
+    tests = new Test[n];
+    mutexes = new mutex[n];
+    vars = new condition_variable[n];
+    isThreadReady = new bool[n];
+    endThreads = false;
 }
 
-bool ThreadHandler::setTestAndStartThread(int index, Test t)
+ThreadHandler::~ThreadHandler()
 {
-    if (index >= number)
+    delete[] tests;
+    delete[] mutexes;
+    delete[] vars;
+    delete[] isThreadReady;
+}
+
+bool ThreadHandler::setTestAtPosition(int position, Test t)
+{
+    if (position >= number)
         return false;
-    tests[index] = t;
+    tests[position] = t;
     return true;
 }
+
+bool ThreadHandler::getMutexAtPosition(int position, mutex &m)
+{
+
+}
+
+bool ThreadHandler::getConditionVariableAtPosition(int position, condition_variable &var)
+{
+
+}
+
+bool ThreadHandler::setThreadAtPositionIsReady(int position)
+{
+
+}
+
+bool ThreadHandler::setThreadAtPositionIsBusy(int position)
+{
+
+}
+
+int ThreadHandler::findFreeThread()
+{
+
+}
+
+void ThreadHandler::stopAllThreads()
+{
+
+}
+
+
 
--- bp/threadhandler.h
+++ bp/threadhandler.h
@@ -1,6 +1,7 @@
 #ifndef THREADHANDLER_H
 #define THREADHANDLER_H
 #include <vector>
+#include <thread>
 #include <condition_variable>
 #include "test.h"
 
@@ -8,12 +9,22 @@ class ThreadHandler
 {
 private:
     int number;
-    std::vector<Test> tests;
-    //std::vector<std::condition_variable> vars;
+    Test* tests;
+    std::mutex* mutexes;
+    std::condition_variable* vars;
+    bool* isThreadReady;
     bool endThreads;
 public:
     ThreadHandler(int n);
-    bool setTestAndStartThread(int index, Test t);
+    ~ThreadHandler();
+    bool setTestAtPosition(int position, Test t);
+    bool getMutexAtPosition(int position, std::mutex& m);
+    bool getConditionVariableAtPosition(int position, std::condition_variable& var);
+    bool setThreadAtPositionIsReady(int position);
+    bool setThreadAtPositionIsBusy(int position);
+    int findFreeThread();
+    void stopAllThreads();
+
 };
 
 #endif // THREADHANDLER_H
