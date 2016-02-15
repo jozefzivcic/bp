59d5aa0694231d14d45ebc1065f122bf7e8a12b1
JozefZivcic <jozefzivcic@gmail.com>
2016-02-15 14:25:32 +0100

    Removed constants.h/cpp from repo

--- bp/constants.cpp
+++
@@ -1,7 +0,0 @@
-#include "constants.h"
-const std::string Constants::DATABASE = "tcp://localhost:3306";
-const std::string Constants::USERNAME = "root";
-const std::string Constants::USER_PASSWORD = "admin";
-const std::string Constants::SCHEMA = "mydb";
-const std::string Constants::NIST = "nist";
-const std::string Constants::PATH_TO_NIST_BINARY = "../tests/run";
