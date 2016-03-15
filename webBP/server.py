b0901ee978fb56cc272607149895e0f42803d786
JozefZivcic <jozefzivcic@gmail.com>
2016-03-15 09:09:18 +0100

    Modified .gitignore and fixed running server

--- webBP/server.py
+++ webBP/server.py
@@ -1,10 +1,10 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import HTTPServer
 
 from myrequesthandler import MyRequestHandler
 
 if __name__ == '__main__':
     server_class = HTTPServer
-    httpd = server_class(("127.0.0.1","8080"),MyRequestHandler)
+    httpd = server_class(("127.0.0.1",8080),MyRequestHandler)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
