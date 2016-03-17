5aef314a490159e7ddf3d4c1c5799d60542b1030
JozefZivcic <jozefzivcic@gmail.com>
2016-03-17 19:17:42 +0100

    Added controller

--- webBP/myrequesthandler.py
+++ webBP/myrequesthandler.py
@@ -1,6 +1,12 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
+from controller import index
+from router import Router
 
 class MyRequestHandler(BaseHTTPRequestHandler):
+
+    """def __init__(self):
+        self.router = Router()
+        self.router.register_controller('/', index)"""
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
