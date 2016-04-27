9156cdf8ce106a9bf7be6bc6e3e0c134bd67a222
JozefZivcic <jozefzivcic@gmail.com>
2016-04-27 20:41:51 +0200

    Changed Content-type for css

--- webBP/controllers/css_controller.py
+++ webBP/controllers/css_controller.py
@@ -3,7 +3,7 @@ import os
 
 def get_bootstrap(handler):
     handler.send_response(200)
-    handler.send_header('Content-type', 'plain')
+    handler.send_header('Content-type', 'text/css')
     handler.end_headers()
     script_dir = os.path.dirname(os.path.realpath(__file__))
     abspath = os.path.join(script_dir, '../views/bootstrap/css/bootstrap.min.css')
@@ -15,7 +15,7 @@ def get_bootstrap(handler):
 
 def get_own_styles(handler):
     handler.send_response(200)
-    handler.send_header('Content-type', 'plain')
+    handler.send_header('Content-type', 'text/css')
     handler.end_headers()
     script_dir = os.path.dirname(os.path.realpath(__file__))
     abspath = os.path.join(script_dir, '../views/styles.css')
