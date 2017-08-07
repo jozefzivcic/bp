876c11998e468aa68ba9e0452f5653bb7f75ce09
Jozef Živčic <jozefzivcic@gmail.com>
2017-08-07 12:09:47 +0200

    Implemented header of results file

--- webBP/nist_statistics/statistics_creator.py
+++ webBP/nist_statistics/statistics_creator.py
@@ -1,4 +1,6 @@
-from os.path import join, dirname, abspath
+from os.path import join, dirname, abspath, exists
+
+from os import makedirs
 
 
 class StatisticsCreator:
@@ -12,17 +14,21 @@ class StatisticsCreator:
             self.template1 = f.read()
         with open(join(this_dir, 'templates', 'template2.txt'), 'r') as f:
             self.template2 = f.read()
+        self.template2 = self.template2[:-1]
         with open(join(this_dir, 'templates', 'template3.txt'), 'r') as f:
             self.template3 = f.read()
 
-    def compute_statistics(self, group_id):
+    def compute_statistics(self, group_id, user_id):
         return
 
-    def prepare_file(self, group_id):
-        file_name = join(self.config_storage.path_to_users_dir, self.config_storage.groups, str(group_id))
-        with open(file_name, 'w') as f:
-            f.write(self.template1)
-            f.write(self.template2)
-            f.write(self.template3)
+    def prepare_file(self, group_id, user_id, file):
+        dir_name = join(self.config_storage.path_to_users_dir, str(user_id), self.config_storage.groups, str(group_id))
+        if not exists(dir_name):
+            makedirs(dir_name)
+        file_name = 'grp_' + str(group_id) + '_f_' + str(file.id)
+        file_loc = join(dir_name, file_name)
+        to_write = self.template1 + self.template2 + ' <' + file.name + '>\n' + self.template3
+        with open(file_loc, 'w') as f:
+            f.write(to_write)
 
 
--- webBP/tests/statistics_creator_test.py
+++ webBP/tests/statistics_creator_test.py
@@ -1,12 +1,30 @@
 import unittest
+from filecmp import cmp
+from os.path import abspath, dirname, join
+from unittest.mock import MagicMock
 
 from logger import Logger
 from nist_statistics.statistics_creator import StatisticsCreator
+from models.file import File
 
 
 class StatCreatorTest(unittest.TestCase):
     def setUp(self):
-        self.stat_creator = StatisticsCreator(None, Logger(), None)
+        this_dir = dirname(abspath(__file__))
+        storage_mock = MagicMock()
+        storage_mock.path_to_users_dir = join(this_dir, 'users')
+        storage_mock.groups = 'groups'
+        self.stat_creator = StatisticsCreator(None, Logger(), storage_mock)
 
     def test_prepare_file(self):
-        self.assertTrue(True, 'Files are not the same')
+        user_id = 4
+        group_id = 5
+        file = File()
+        file.id = 6
+        file.name = 'TestFile.txt'
+        self.stat_creator.prepare_file(group_id, user_id, file)
+        this_dir = dirname(abspath(__file__))
+        created_file = join(this_dir, 'users', str(user_id), 'groups', str(group_id), 'grp_' + str(group_id) + '_f_' +
+                            str(file.id))
+        another_file = join(this_dir, 'good_file.txt')
+        self.assertTrue(cmp(created_file, another_file), 'Files are not the same')
