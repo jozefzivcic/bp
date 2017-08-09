3de28c530a984d945d8ae064356e7a83e56ecf7a
Jozef Živčic <jozefzivcic@gmail.com>
2017-08-09 16:08:45 +0200

    Implemented another test

--- webBP/tests/p_value_counter_test.py
+++ webBP/tests/p_value_counter_test.py
@@ -6,6 +6,7 @@ from nist_statistics.PValueCounter import PValueCounter
 this_dir = dirname(abspath(__file__))
 file1 = join(this_dir, 'test_files', 'pvalues1.txt')
 file2 = join(this_dir, 'test_files', 'pvalues2.txt')
+file3 = join(this_dir, 'test_files', 'pvalues3.txt')
 
 
 class PValueCounterTest(unittest.TestCase):
@@ -40,3 +41,15 @@ class PValueCounterTest(unittest.TestCase):
         self.assertEqual(another_counter.get_proportions(), 2 / 5,
                          'Proportions are not the same for the file ' + file2 +
                          ' with alpha ' + '0.05')
+
+    def test_two_files_load(self):
+        file1_arr = [0, 1, 0, 1, 3, 2, 1, 2, 0, 0]
+        file3_arr = [0, 1, 1, 1, 0, 1, 0, 2, 2, 2]
+        res = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for i in range(0, 10):
+            res[i] = file1_arr[i] + file3_arr[i]
+        self.counter.count_p_values_in_file(file1)
+        self.counter.count_p_values_in_file(file3)
+        for i in range(0, 10):
+            self.assertTrue(self.counter.arr[i] == res[i], 'Counter.arr[' + str(i) + '] and res[' + str(i) +
+                            '] are not the same')
