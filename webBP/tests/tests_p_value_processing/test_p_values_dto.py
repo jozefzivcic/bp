6345ee0f7228394a9993500bf7db3b11a671bd34
Jozef Živčic <jozefzivcic@gmail.com>
2017-12-16 11:39:46 +0100

    Another tests on DTO

--- webBP/tests/tests_p_value_processing/test_p_values_dto.py
+++ webBP/tests/tests_p_value_processing/test_p_values_dto.py
@@ -4,16 +4,55 @@ from tests.data_for_tests.common_data import dict_for_test_13, dict_for_test_14
 from p_value_processing.p_values_dto import PValuesDto
 
 
-class TestPValuesDto(TestCase):
+class TestPValuesDtoOnTest13(TestCase):
     def setUp(self):
-        self.dto_13 = PValuesDto(dict_for_test_13)
-        self.dto_14 = PValuesDto(dict_for_test_14)
+        self.dto = PValuesDto(dict_for_test_13)
 
     def test_results(self):
         expected_p_values = dict_for_test_13['results']
-        ret = self.dto_13.get_results_p_values()
+        ret = self.dto.get_results_p_values()
         self.assertEqual(expected_p_values, ret)
 
     def test_data_raise_exception(self):
-        with self.assertRaises(ValueError):
-            self.dto_13.get_data_p_values(1)
+        data_num = 1
+        with self.assertRaises(ValueError) as context:
+            self.dto.get_data_p_values(data_num)
+            self.assertTrue(str(data_num) in str(context.exception))
+
+
+class TestPValuesDtoOnTest14(TestCase):
+    def setUp(self):
+        self.dto = PValuesDto(dict_for_test_14)
+
+    def test_results(self):
+        expected_p_values = dict_for_test_14['results']
+        ret = self.dto.get_results_p_values()
+        self.assertEqual(expected_p_values, ret)
+
+    def test_data_1(self):
+        expected_p_values = dict_for_test_14['data1']
+        ret = self.dto.get_data_p_values(1)
+        self.assertEqual(expected_p_values, ret)
+
+    def test_data_2(self):
+        expected_p_values = dict_for_test_14['data2']
+        ret = self.dto.get_data_p_values(2)
+        self.assertEqual(expected_p_values, ret)
+
+    def test_data_3(self):
+        data_num = 3
+        with self.assertRaises(ValueError) as context:
+            self.dto.get_data_p_values(data_num)
+            self.assertTrue(str(data_num) in str(context.exception))
+
+    def test_data_0(self):
+        data_num = 0
+        with self.assertRaises(ValueError) as context:
+            self.dto.get_data_p_values(data_num)
+            self.assertTrue(str(data_num) in str(context.exception))
+
+    def test_data_not_number(self):
+        data_num = 'string'
+        with self.assertRaises(ValueError) as context:
+            self.dto.get_data_p_values(data_num)
+            self.assertTrue(str(data_num) in str(context.exception))
