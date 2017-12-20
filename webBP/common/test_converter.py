a9e6a73dee9bd4597aacf3178a3958340e627057
Jozef Živčic <jozefzivcic@gmail.com>
2017-12-20 12:44:26 +0100

    Test converter new method + test on it

--- webBP/common/test_converter.py
+++ webBP/common/test_converter.py
@@ -14,3 +14,19 @@ class TestConverter:
             else:
                 my_dict[test.file_id] = [test]
         return my_dict
+
+    def get_test_ids_for_files(self, tests: list):
+        """
+        Converts array of tests into dictionary. This dictionary contains id's of files as keys. For each key,
+        there is an array of test ids, that are associated with the file - i.e. tests which have tests.id_file the same
+        as files.id in database.
+        :param arr: Array of tests
+        :return: Dictionary of file_ids and associated test ids.
+        """
+        my_dict = {}
+        for test in tests:
+            if test.file_id in my_dict:
+                my_dict[test.file_id].append(test.id)
+            else:
+                my_dict[test.file_id] = [test.id]
+        return my_dict
--- webBP/tests/tests_common/test_test_converter.py
+++ webBP/tests/tests_common/test_test_converter.py
@@ -17,26 +17,28 @@ test_table = 'nist'
 class TestTestConverter(TestCase):
     def setUp(self):
         self.test_converter = TestConverter()
+        self.test1 = Test()
+        self.test1.id = 5
+        self.test1.file_id = file1_id
+        self.test1.user_id = user_id
+        self.test1.time_of_add = time_of_add
+        self.test1.test_table = test_table
+
+        self.test2 = copy.deepcopy(self.test1)
+        self.test2.id = 6
+
+        self.test3 = copy.deepcopy(self.test1)
+        self.test3.id = 14
+        self.test3.file_id = file2_id
 
     def test_convert(self):
-        test1 = Test()
-        test1.id = 5
-        test1.file_id = file1_id
-        test1.user_id = user_id
-        test1.time_of_add = time_of_add
-        test1.test_table = test_table
-
-        test2 = copy.deepcopy(test1)
-        test2.id = 6
-
-        test3 = copy.deepcopy(test1)
-        test3.id = 14
-        test3.file_id = file2_id
-
-        test_arr = [test1, test2, test3]
-
+        test_arr = [self.test1, self.test2, self.test3]
         returned_dict = self.test_converter.get_tests_for_files(test_arr)
+        expected_dict = {file1_id: [self.test1, self.test2], file2_id: [self.test3]}
+        self.assertDictEqual(returned_dict, expected_dict, 'Returned and expected dictionaries are not the same')
 
-        expected_dict = {file1_id: [test1, test2], file2_id: [test3]}
-
+    def test_get_test_ids_for_files(self):
+        test_arr = [self.test1, self.test2, self.test3]
+        returned_dict = self.test_converter.get_test_ids_for_files(test_arr)
+        expected_dict = {file1_id: [self.test1.id, self.test2.id], file2_id: [self.test3.id]}
         self.assertDictEqual(returned_dict, expected_dict, 'Returned and expected dictionaries are not the same')
