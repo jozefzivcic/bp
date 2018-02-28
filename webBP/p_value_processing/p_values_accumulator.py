78958c50c05398d7cb6afcb644dcab2256c9b5c7
Jozef Živčic <jozefzivcic@gmail.com>
2018-02-28 11:38:25 +0100

    Accumulator get_dto_for_test safe version

--- webBP/p_value_processing/p_values_accumulator.py
+++ webBP/p_value_processing/p_values_accumulator.py
@@ -11,9 +11,13 @@ class PValuesAccumulator:
         self._objects[test_id] = dto
 
     def get_dto_for_test(self, test_id: int) -> PValuesDto:
-        if not test_id in self._objects:
+        ret = self.get_dto_for_test_safe(test_id)
+        if ret is None:
             raise ValueError('No DTO for test with id: ' + str(test_id))
-        return self._objects[test_id]
+        return ret
+
+    def get_dto_for_test_safe(self, test_id: int) -> PValuesDto:
+        return self._objects.get(test_id, None)
 
     def get_all_test_ids(self) -> list:
         return sorted(self._objects.keys())
--- webBP/tests/tests_p_value_processing/test_p_values_accumulator.py
+++ webBP/tests/tests_p_value_processing/test_p_values_accumulator.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from copy import deepcopy
+
 from p_value_processing.p_values_accumulator import PValuesAccumulator
 from p_value_processing.p_values_dto import PValuesDto
 
@@ -10,22 +12,23 @@ class TestPValuesAccumulator(TestCase):
         self.dto2 = PValuesDto({'results': [0.3, 0.4, 0.5, 0.6], 'data1': [0.3, 0.5], 'data2': [0.4, 0.6]})
         self.test1_id = 789
         self.test2_id = 1234
+        self.non_existing_test_id = 1235
         self.accumulator = PValuesAccumulator()
 
     def test_add_one_dto(self):
         self.accumulator.add(self.test1_id, self.dto1)
         ret = self.accumulator.get_dto_for_test(self.test1_id)
-        self.assertEqual(self.dto1, ret)
+        self.assertEqual(deepcopy(self.dto1), ret)
 
     def test_add_two_dtos(self):
         self.accumulator.add(self.test1_id, self.dto1)
         self.accumulator.add(self.test2_id, self.dto2)
 
         ret = self.accumulator.get_dto_for_test(self.test1_id)
-        self.assertEqual(self.dto1, ret)
+        self.assertEqual(deepcopy(self.dto1), ret)
 
         ret = self.accumulator.get_dto_for_test(self.test2_id)
-        self.assertEqual(self.dto2, ret)
+        self.assertEqual(deepcopy(self.dto2), ret)
 
     def test_add_with_same_test_id(self):
         self.accumulator.add(self.test1_id, self.dto1)
@@ -38,6 +41,21 @@ class TestPValuesAccumulator(TestCase):
         with self.assertRaises(ValueError) as context:
             self.accumulator.get_dto_for_test(self.test2_id)
         self.assertTrue(str(self.test2_id) in str(context.exception))
+
+    def test_get_dto_for_test_safe_existing_test_id(self):
+        self.accumulator.add(self.test1_id, self.dto1)
+        self.accumulator.add(self.test2_id, self.dto2)
+        ret = self.accumulator.get_dto_for_test_safe(self.test1_id)
+        self.assertEqual(deepcopy(self.dto1), ret)
+
+        ret = self.accumulator.get_dto_for_test_safe(self.test2_id)
+        self.assertEqual(deepcopy(self.dto2), ret)
+
+    def test_get_dto_for_test_safe_non_existing_test_id(self):
+        self.accumulator.add(self.test1_id, self.dto1)
+        self.accumulator.add(self.test2_id, self.dto2)
+        ret = self.accumulator.get_dto_for_test_safe(self.non_existing_test_id)
+        self.assertIsNone(ret)
 
     def test_get_all_test_ids(self):
         self.accumulator.add(self.test2_id, self.dto2)
