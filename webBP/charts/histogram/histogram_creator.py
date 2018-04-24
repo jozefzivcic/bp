5126dacaf89334d7ea7f3f9e0e5ec64e1e49f0af
Jozef Živčic <jozefzivcic@gmail.com>
2018-04-24 20:28:48 +0200

    Histogram creator

--- webBP/charts/histogram/histogram_creator.py
+++ webBP/charts/histogram/histogram_creator.py
@@ -2,27 +2,44 @@ from os.path import join
 
 from charts.chart_info import ChartInfo
 from charts.chart_type import ChartType
+from charts.charts_storage import ChartsStorage
+from charts.data_source_info import DataSourceInfo
 from charts.histogram.data_for_histogram_creator import DataForHistogramCreator
 from charts.histogram.histogram_drawer import HistogramDrawer
 from charts.histogram.historam_extractor import HistogramExtractor
+from charts.tests_in_chart import TestsInChart
+from p_value_processing.p_value_sequence import PValueSequence
+from p_value_processing.p_values_file_type import PValuesFileType
 
 
 class HistogramCreator:
     def __init__(self):
         self._extractor = HistogramExtractor()
-        self.drawer = HistogramDrawer()
+        self._drawer = HistogramDrawer()
 
-    def create_histogram(self, data: DataForHistogramCreator) -> ChartInfo:
+    def create_histogram(self, data: DataForHistogramCreator) -> ChartsStorage:
         self.check_input(data)
-        file = self.get_file_name_for_chart(data.directory, data.file_id)
+        storage = ChartsStorage()
         extracted_data = self._extractor.get_data_from_accumulator(data.acc, data.histogram_dto)
         ex_data_list = extracted_data.get_all_data()
-        first_elem = ex_data_list[0]
-        self.drawer.draw_chart(first_elem[1], file)
-        return ChartInfo(first_elem[0], file, ChartType.HISTOGRAM, data.file_id)
+        for ds_info, data_for_drawer, info, err in ex_data_list:
+            file_name = self.get_file_name_for_chart(data.directory, ds_info)
+            self._drawer.draw_chart(data_for_drawer, file_name)
+            ch_info = ChartInfo(ds_info, file_name, ChartType.HISTOGRAM, data.file_id)
+            storage.add_chart_info(ch_info, info, err)
+        storage.add_infos_from_chart(ChartType.HISTOGRAM, extracted_data.get_all_infos())
+        storage.add_errors_from_chart(ChartType.HISTOGRAM, extracted_data.get_all_errs())
+        return storage
 
-    def get_file_name_for_chart(self, directory, file_id):
-        file_name = 'histogram_for_file_' + str(file_id) + '.png'
+    def get_file_name_for_chart(self, directory: str, ds_info: DataSourceInfo):
+        if ds_info.tests_in_chart == TestsInChart.MULTIPLE_TESTS:
+            file_name = 'hist_mult_t_first_tid_{}.png'.format(ds_info.p_value_sequence[0].test_id)
+            return join(directory, file_name)
+        seq = ds_info.p_value_sequence  # type: PValueSequence
+        seq_str = 't{}'.format(seq.test_id)
+        if seq.p_values_file == PValuesFileType.DATA:
+            seq_str += '_data{}'.format(seq.data_num)
+        file_name = 'hist_single_{}'.format(seq_str)
         return join(directory, file_name)
 
     def check_input(self, data: DataForHistogramCreator):
--- webBP/tests/tests_charts/tests_histogram/test_histogram_creator.py
+++ webBP/tests/tests_charts/tests_histogram/test_histogram_creator.py
@@ -5,11 +5,17 @@ from unittest import TestCase
 from shutil import rmtree
 
 from charts.chart_type import ChartType
+from charts.charts_storage_item import ChartsStorageItem
+from charts.data_source_info import DataSourceInfo
 from charts.histogram.data_for_histogram_creator import DataForHistogramCreator
 from charts.histogram.histogram_creator import HistogramCreator
 from charts.dto.histogram_dto import HistogramDto
+from charts.tests_in_chart import TestsInChart
+from enums.hist_for_tests import HistForTests
+from p_value_processing.p_value_sequence import PValueSequence
 from p_value_processing.p_values_accumulator import PValuesAccumulator
 from p_value_processing.p_values_dto import PValuesDto
+from p_value_processing.p_values_file_type import PValuesFileType
 
 this_dir = dirname(abspath(__file__))
 working_dir = join(this_dir, 'working_dir_histogram_creator')
@@ -27,7 +33,8 @@ class TestHistogramCreator(TestCase):
         acc.add(456, dto1)
         acc.add(654, dto2)
 
-        hist_dto = HistogramDto('intervals', 'number of p-values', 'histogram')
+        hist_dto = HistogramDto('intervals', 'number of p-values', 'histogram', [HistForTests.ALL_TESTS,
+                                                                                 HistForTests.INDIVIDUAL_TESTS])
         self.data_for_creator = DataForHistogramCreator(hist_dto, acc, working_dir, 789)
         self.creator = HistogramCreator()
 
@@ -36,14 +43,53 @@ class TestHistogramCreator(TestCase):
             rmtree(working_dir)
 
     def test_create_p_values_chart(self):
-        ch_info = self.creator.create_histogram(self.data_for_creator)
-        self.assertTrue(exists(ch_info.path_to_chart))
+        ch_storage = self.creator.create_histogram(self.data_for_creator)
+        cs_items = ch_storage.get_all_items()
+        self.assertEqual(3, len(cs_items))
+
+        seq1 = PValueSequence(456, PValuesFileType.RESULTS)
+        seq2 = PValueSequence(654, PValuesFileType.RESULTS)
+
+        ch_item = cs_items[0]  # type: ChartsStorageItem
+        ch_info = ch_item.ch_info
+        self.assertTrue(TestsInChart.SINGLE_TEST, ch_info.ds_info.tests_in_chart)
+        self.assertTrue(seq1, ch_info.ds_info.p_value_sequence)
         self.assertEqual(ChartType.HISTOGRAM, ch_info.chart_type)
         self.assertEqual(self.data_for_creator.file_id, ch_info.file_id)
 
-    def test_get_file_name_for_chart(self):
-        expected = join(working_dir, 'histogram_for_file_' + str(self.data_for_creator.file_id) + '.png')
-        ret = self.creator.get_file_name_for_chart(working_dir, self.data_for_creator.file_id)
+        ch_item = cs_items[1]  # type: ChartsStorageItem
+        ch_info = ch_item.ch_info
+        self.assertTrue(TestsInChart.SINGLE_TEST, ch_info.ds_info.tests_in_chart)
+        self.assertTrue(seq2, ch_info.ds_info.p_value_sequence)
+        self.assertEqual(ChartType.HISTOGRAM, ch_info.chart_type)
+        self.assertEqual(self.data_for_creator.file_id, ch_info.file_id)
+
+        ch_item = cs_items[2]  # type: ChartsStorageItem
+        ch_info = ch_item.ch_info
+        self.assertTrue(TestsInChart.MULTIPLE_TESTS, ch_info.ds_info.tests_in_chart)
+        self.assertTrue([seq1, seq2], ch_info.ds_info.p_value_sequence)
+        self.assertEqual(ChartType.HISTOGRAM, ch_info.chart_type)
+        self.assertEqual(self.data_for_creator.file_id, ch_info.file_id)
+
+    def test_get_file_name_multiple_tests(self):
+        seqcs = [PValueSequence(1, PValuesFileType.RESULTS), PValueSequence(2, PValuesFileType.DATA, 2)]
+        ds_info = DataSourceInfo(TestsInChart.MULTIPLE_TESTS, seqcs)
+        expected = join(working_dir, 'hist_mult_t_first_tid_1.png')
+        ret = self.creator.get_file_name_for_chart(working_dir, ds_info)
+        self.assertEqual(expected, ret)
+
+    def test_get_file_name_single_test_results(self):
+        seq = PValueSequence(1, PValuesFileType.RESULTS)
+        ds_info = DataSourceInfo(TestsInChart.SINGLE_TEST, seq)
+        expected = join(working_dir, 'hist_single_t1')
+        ret = self.creator.get_file_name_for_chart(working_dir, ds_info)
+        self.assertEqual(expected, ret)
+
+    def test_get_file_name_single_test_data(self):
+        seq = PValueSequence(1, PValuesFileType.DATA, 2)
+        ds_info = DataSourceInfo(TestsInChart.SINGLE_TEST, seq)
+        expected = join(working_dir, 'hist_single_t1_data2')
+        ret = self.creator.get_file_name_for_chart(working_dir, ds_info)
         self.assertEqual(expected, ret)
 
     def test_check_input_none_data(self):
